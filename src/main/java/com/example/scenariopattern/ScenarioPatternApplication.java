474ae9935fe5e12614de36d99927f04250f515bf
Javed Bagwan <javed460@gmail.com>
2023-12-27 08:25:44 +0530

    Initial project structure

---
+++ src/main/java/com/example/scenariopattern/ScenarioPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.scenariopattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenarioPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scenariopattern/ScenarioPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scenariopattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenarioPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
